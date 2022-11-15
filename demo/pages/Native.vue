5fb953b7daf63cfc9216eeb44975e36531fde5af
Narayan Adhikary <wovosoft@gmail.com>
2022-11-15 19:57:03 +0600

    Started Developing Native UI Components

---
+++ demo/pages/Native.vue
@@ -0,0 +1,13 @@
+<template>
+    <Container>
+        <NativeCollapse
+            summary="Hello Native Summery"
+            details="Hello Native Details"
+        />
+    </Container>
+</template>
+
+<script lang="ts" setup>
+import {Container, NativeCollapse} from "../../src/index.js";
+
+</script>
---
+++ src/components/Native/NativeCollapse.vue
@@ -0,0 +1,23 @@
+<template>
+    <details>
+        <summary :class="summaryClass" :style="summaryStyle">
+            <slot name="summery">{{ summary }}</slot>
+        </summary>
+        <main :class="detailsClass" :style="detailsStyle">
+            <slot>{{ details }}</slot>
+        </main>
+    </details>
+</template>
+
+<script lang="ts" setup>
+import {PropType} from "vue";
+
+const props = defineProps({
+    summary: String as PropType<string>,
+    details: String as PropType<string>,
+    detailsClass: {default: null},
+    summaryClass: {default: null},
+    detailsStyle: {default: null},
+    summaryStyle: {default: null},
+});
+</script>
