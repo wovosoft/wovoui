1481cb36355810146fc69ce33d11c6fedec1430c
Narayan Adhikary <wovosoft@gmail.com>
2022-11-15 20:32:15 +0600

    Native Components

---
+++ src/components/Native/NativeProgress.vue
@@ -0,0 +1,14 @@
+<template>
+    <progress :value="value" :max="max">
+        <slot/>
+    </progress>
+</template>
+
+<script lang="ts" setup>
+import {PropType} from "vue";
+
+const props = defineProps({
+    value: [String, Number] as PropType<string | number>,
+    max: [String, Number] as PropType<string | number>,
+});
+</script>
