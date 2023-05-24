4ae73446d02562396369a1c27e346d908e72c383
Narayan Adhikary <wovosoft@gmail.com>
2023-05-24 18:18:52 +0600

    Updated Radio Group Component

--- demo/pages/Index.vue
+++ demo/pages/Index.vue
@@ -3,13 +3,33 @@
         <h3>wovoui</h3>
         wovoui is an impmentation of Bootstrap 5 components in vue 3. It is developed by
         Narayan Adhikary at wovosoft
+        <RadioGroup
+            inline
+            v-model="rg"
+            :options="[{text:'Hello',value:'hello'},{text: 'World',value: 'world'}]"
+            text-field="text"
+            value-field="value"
+        />
+        <RadioGroup
+            inline
+            v-model="rg"
+            :options="[{text: 'World',value: 'world'},{text:'Hello',value:'hello'}]"
+            text-field="text"
+            value-field="value"
+        />
+        <span>{{rg}}</span>
 
-<!--        <h3>Spinner</h3>-->
-<!--        <Spinner></Spinner>-->
+        <!--        <h3>Spinner</h3>-->
+        <!--        <Spinner></Spinner>-->
     </Container>
 </template>
 
 <script lang="ts" setup>
 import {Container} from "../../src";
 import Spinner from "../../src/components/Indicators/Spinner";
+import RadioGroup from "../../src/components/Form/RadioGroup.vue";
+import Radio from "../../src/components/Form/Radio.vue";
+import {ref} from "vue";
+
+const rg = ref('hello')
 </script>
--- src/components/Form/RadioGroup.vue
+++ src/components/Form/RadioGroup.vue
@@ -1,11 +1,12 @@
 <template>
     <div role="group">
         <Radio
+            :name="theName"
             v-for="(option,option_key) in options"
             :key="option_key"
             :inline="inline"
             :disabled="option.disabled===true"
-            @update:modelValue="v=>$emit('update:modelValue',v)"
+            v-model="model"
             :value="valueField?option[valueField]:option">
             {{ textField ? option[textField] : option }}
         </Radio>
@@ -13,15 +14,32 @@
 </template>
 
 <script lang="ts" setup>
-import {PropType} from "vue";
-import {makeBoolean, makeString} from "../../composables/useProps";
+import {computed, PropType, ref} from "vue";
+import {makeString} from "../../composables/useProps";
 import Radio from "./Radio.vue";
+import {uid} from "../../composables/useHelpers";
 
 const props = defineProps({
     modelValue: {default: null},
     options: {type: Array as PropType<any>, default: () => ([])},
     textField: makeString(null),
     valueField: makeString(null),
-    inline: makeBoolean(false),
+    inline: {
+        type: Boolean as PropType<boolean>,
+        default: false
+    },
+    name: makeString(null)
+});
+
+const theName = computed(() => props.name || "radio-group-" + uid());
+const emit = defineEmits<{
+    (e: 'update:modelValue', value: any)
+}>();
+
+const model = computed({
+    get: () => props.modelValue,
+    set: (value) => {
+        emit('update:modelValue', value);
+    }
 });
 </script>
