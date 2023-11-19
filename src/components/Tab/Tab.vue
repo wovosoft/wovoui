bd6057785752988c33463c284aa36aeaf1a360dd
Narayan Adhikary <wovosoft@gmail.com>
2023-11-19 12:40:44 +0600

    Minor Improvements

--- src/components/Tab/Tab.vue
+++ src/components/Tab/Tab.vue
@@ -1,106 +1,106 @@
 <template>
-    <div :class="classes"
-         ref="theRoot"
-         role="tabpanel"
-         tabindex="0">
-        <slot></slot>
-    </div>
+	<div :class="classes"
+	     ref="theRoot"
+	     role="tabpanel"
+	     tabindex="0">
+		<slot></slot>
+	</div>
 </template>
 
 <script lang="ts" setup>
-import {computed, getCurrentInstance, inject, onBeforeUnmount, onMounted, reactive, ref, watch} from "vue";
+import {computed, getCurrentInstance, inject, onBeforeUnmount, reactive, ref, useModel, watch} from "vue";
 import {getTransitionDurationFromElement, makeBoolean, makeString} from "@/composables";
-import {registerTabType, unregisterTabType} from "./index";
+import {registerTabInjection, unregisterTabInjection} from "./index";
 
 const props = defineProps({
-    active: makeBoolean(false),
-    title: makeString()
+	active: makeBoolean(false),
+	title: makeString()
 });
-
-const emit = defineEmits<{
-    (e: 'update:active', value: boolean): void
-}>();
 
 /**
  * This state is handled by parent Component Tabs.
  * Which is done by updateVisibility method.
  */
-const visible = ref<boolean>(props.active);
+const visible = useModel(props, 'active', {local: true});
 const theRoot = ref<HTMLElement>();
 
 watch(visible, value => {
-    if (!theRoot?.value) {
-        return;
-    }
-
-    emit("update:active", value);
-    if (value) {
-        /**
-         * @link https://github.com/twbs/bootstrap/blob/main/js/src/tab.js#L111-L126
-         */
-        states.active = true;
-        states.ariaSelected = true;
-        states.tabindex = null;
-
-        setTimeout(
-            () => states.show = true,
-            getTransitionDurationFromElement(theRoot.value)
-        );
-    } else {
-        /**
-         * First Active class is removed
-         * @link https://github.com/twbs/bootstrap/blob/main/js/src/tab.js#L133
-         */
-        states.active = false;
-        theRoot.value?.blur();
-
-        setTimeout(() => {
-            states.show = false;
-            states.ariaSelected = false;
-            states.tabindex = -1;
-        }, getTransitionDurationFromElement(theRoot.value));
-    }
+	if (!theRoot?.value) {
+		return;
+	}
+	
+	if (value) {
+		/**
+		 * @link https://github.com/twbs/bootstrap/blob/main/js/src/tab.js#L111-L126
+		 */
+		states.active = true;
+		states.ariaSelected = true;
+		states.tabindex = null;
+		
+		setTimeout(
+			() => states.show = true,
+			getTransitionDurationFromElement(theRoot.value)
+		);
+	} else {
+		/**
+		 * First Active class is removed
+		 * @link https://github.com/twbs/bootstrap/blob/main/js/src/tab.js#L133
+		 */
+		states.active = false;
+		theRoot.value?.blur();
+		
+		setTimeout(() => {
+			states.show = false;
+			states.ariaSelected = false;
+			states.tabindex = -1;
+		}, getTransitionDurationFromElement(theRoot.value));
+	}
 });
 
 /**
  * Tab Class states
  */
 const states = reactive<{
-    active: boolean,
-    show: boolean,
-    ariaSelected: boolean | null,
-    tabindex: number | null
+	active: boolean,
+	show: boolean,
+	ariaSelected: boolean | null,
+	tabindex: number | null
 }>({
-    active: props.active,
-    show: props.active,
-    ariaSelected: props.active,
-    tabindex: props.active ? null : -1,
+	active: props.active,
+	show: props.active,
+	ariaSelected: props.active,
+	tabindex: props.active ? null : -1,
 });
 
 const classes = computed(() => [
-    "tab-pane", "fade", {
-        "show": states.show,
-        "active": states.active,
-        "card-body": isCardTabs.value
-    }
+	"tab-pane", "fade", {
+		"show": states.show,
+		"active": states.active,
+		"card-body": isCardTabs.value
+	}
 ]);
 
 
 //injections
-const registerTab = inject('registerTab') as registerTabType;
-const unregisterTab = inject('unregisterTab') as unregisterTabType;
+const registerTab = inject(registerTabInjection);
+const unregisterTab = inject(unregisterTabInjection);
 
 //when parent Tabs component has card prop set to true
 const isCardTabs = inject('isCardTabs') as { value: boolean };
 
-registerTab({
-    uid: getCurrentInstance()?.uid,
-    updateVisibility: (value: boolean) => visible.value = value,
-    title: props.title,
-    states: states
-});
+if (registerTab) {
+	registerTab({
+		uid: getCurrentInstance()?.uid,
+		updateVisibility: (value: boolean) => visible.value = value,
+		title: props.title,
+		visible: visible.value,
+		states: states
+	});
+}
 
 //unsubscribe from parent tabs
-onBeforeUnmount(() => unregisterTab(getCurrentInstance()?.uid));
+if (unregisterTab) {
+	onBeforeUnmount(() => unregisterTab(getCurrentInstance()?.uid));
+}
 
 </script>
--- src/components/Tab/index.ts
+++ src/components/Tab/index.ts
@@ -1,15 +1,17 @@
+import {InjectionKey} from "vue";
+
 export {default as Tabs} from "./Tabs.vue";
 export {default as Tab} from "./Tab.vue";
 export {default as TabContent} from "./TabContent.vue";
 
-export type registerTabType = (item: TabMapItem) => TabMapItem[]
-export type unregisterTabType = (item: number | undefined) => TabMapItem[]
+export const registerTabInjection = Symbol('Register Tab Item to Tabs') as InjectionKey<(item: TabMapItem) => any>;
+export const unregisterTabInjection = Symbol("Unregister Tab Item from Tabs") as InjectionKey<(item: number | undefined) => any>;
 
 export type TabMapItem = {
     uid?: number | string;
     updateVisibility?: (state: boolean) => void,
     title?: string;
-    // visible?: boolean;
+    visible?: boolean;
     states?: {
         active: boolean;
         show: boolean;
