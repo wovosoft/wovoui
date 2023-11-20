c0af3412088c682f25dbeb8940c0fa84fc968028
Narayan Adhikary <wovosoft@gmail.com>
2023-11-20 16:42:05 +0600

    Updated

--- src/demo/pages/Buttons.vue
+++ src/demo/pages/Buttons.vue
@@ -1,32 +1,31 @@
 <template>
-    <Container>
-        <h3>Default Buttons</h3>
-        <Button>Default Button</Button>
-
-        <h3 class="my-3">Color Variants</h3>
-        <Button v-for="c in colors" :variant="c" class="me-3">{{ c }}</Button>
-
-        <h3 class="my-3">Outline Colors</h3>
-        <Button v-for="c in colors" outline :variant="c" class="me-3">{{ c }}</Button>
-
-        <h3 class="my-3">Sizes</h3>
-        <Button v-for="size in sizesOf('button')" class="me-3" :size="size">
-            Button {{ size }}
-        </Button>
-
-        <h3 class="my-3">Disabled Button</h3>
-        <Button disabled>Disabled</Button>
-        <Button disabled class="mx-3" variant="primary">Disabled</Button>
-        <Button disabled variant="warning">Disabled</Button>
-        <h3>Active Button</h3>
-        <Button size="sm" active>Active</Button>
-
-        <ButtonClose white>Test</ButtonClose>
-    </Container>
+	<Container>
+		<h3>Default Buttons</h3>
+		<Button>Default Button</Button>
+		
+		<h3 class="my-3">Color Variants</h3>
+		<Button v-for="c in colors" :variant="c" class="me-3">{{ c }}</Button>
+		
+		<h3 class="my-3">Outline Colors</h3>
+		<Button v-for="c in colors" outline :variant="c" class="me-3">{{ c }}</Button>
+		
+		<h3 class="my-3">Sizes</h3>
+		<Button v-for="size in sizesOf('button')" class="me-3" :size="size">
+			Button {{ size }}
+		</Button>
+		
+		<h3 class="my-3">Disabled Button</h3>
+		<Button disabled>Disabled</Button>
+		<Button disabled class="mx-3" variant="primary">Disabled</Button>
+		<Button disabled variant="warning">Disabled</Button>
+		<h3>Active Button</h3>
+		<Button size="sm" active>Active</Button>
+		
+		<ButtonClose white>Test</ButtonClose>
+	</Container>
 </template>
 
 <script lang="ts" setup>
-import {Button, Container} from "@/index";
+import {Button, ButtonClose, Container} from "@/components";
 import {colors, sizesOf} from "../assets";
-import ButtonClose from "@/components/Button/ButtonClose";
 </script>
