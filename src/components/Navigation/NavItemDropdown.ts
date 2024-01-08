65a23a95e6d01127c4e2c17d240b03415b97d28b
wovosoft <wovosoft@gmail.com>
2024-01-08 17:02:48 +0600

    Fixed Missing Menu class

--- src/components/Navigation/NavItemDropdown.ts
+++ src/components/Navigation/NavItemDropdown.ts
@@ -12,7 +12,7 @@ export default defineComponent({
     },
     setup(props, {slots}) {
         return () => h(Dropdown, {
-            class: ["nav-item"],
+            class: ["nav-item", props.menuClass],
             ...props
         }, () => slots.default?.())
     }
