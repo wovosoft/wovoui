dfbb5a51f39e8fc0e268728abfd63ccefbec9842
wovosoft <wovosoft@gmail.com>
2026-07-07 02:27:12 +0600

    build: disable preserveModules to avoid emitting virtual query-parameter files

--- vite.config.ts
+++ vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
                 /^prismjs(\/.*)?$/,
             ],
             output: {
-                preserveModules: true,
+                preserveModules: false,
                 preserveModulesRoot: 'src',
                 globals: {
                     vue: 'Vue',
