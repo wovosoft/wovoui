c2431816ed1e9341f39187347187dab72723de99
wovosoft <wovosoft@gmail.com>
2026-07-12 01:46:33 +0600

    Fixed minor Isssue

--- src/components/Navigation/NavLink.vue
+++ src/components/Navigation/NavLink.vue
@@ -1,39 +1,35 @@
 <template>
-    <template v-if="isVueRouterInstalled && to">
-        <component is="router-link" :to="to" :class="classes" v-bind="linkAttributes">
-            <slot></slot>
-        </component>
-    </template>
-    <component v-else :is="tag" :class="classes" v-bind="linkAttributes">
-        <slot></slot>
+  <template v-if="isVueRouterInstalled && to">
+    <component is="router-link" :to="to" :class="classes" v-bind="linkAttributes">
+      <slot></slot>
     </component>
+  </template>
+  <component v-else :is="tag" :class="classes" v-bind="linkAttributes">
+    <slot></slot>
+  </component>
 </template>
 
 <script lang="ts" setup>
 import {type AnchorHTMLAttributes, computed, getCurrentInstance} from "vue";
 import {type NavLinkProps} from "@/components";
 
-type linkAttributesType = {
-    href?: AnchorHTMLAttributes['href'];
-    ariaCurrent?: string;
-    target?: AnchorHTMLAttributes['target'];
-}
 
 const props = withDefaults(defineProps<NavLinkProps>(), {
-    tag: 'a',
+  tag: 'a',
 });
 
 const isVueRouterInstalled = computed(() => {
-    return !!getCurrentInstance()?.appContext.config.globalProperties.$router;
+  return !!getCurrentInstance()?.appContext.config.globalProperties.$router;
 });
 
 const classes = computed(() => ["nav-link", {
-    active: props.active
+  active: props.active
 }]);
 
 const linkAttributes = computed(() => ({
-    href: props.tag === "a" ? props.href : null,
-    ariaCurrent: props.ariaCurrent,
-    target: (props.target === "a" || props.href) ? props.target : null
+  href: props.tag === "a" ? props.href : null,
+  type: props.tag === "button" ? 'button' : null,
+  ariaCurrent: props.ariaCurrent,
+  target: (props.target === "a" || props.href) ? props.target : null
 }));
 </script>
